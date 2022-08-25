3bb1ce8a9dd566890091a777ba6ddbc6bf520ef4
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2022-08-25 15:59:50 -0600

    Modifies usage of renderer and generator to match new pattern

--- src/main.js
+++ src/main.js
@@ -1,22 +1,24 @@
+const Generator = require('./generators/generator.js');
 const NodeMazeGenerator = require('./generators/maze.js');
 const Renderer = require('./renderer.js');
 const RoomGenerator = require('./generators/room.js');
-const R = new Renderer(new NodeMazeGenerator({
-    width: 20,
-    height: 20,
-    generators: [
-        {
-            generator: RoomGenerator,
-            options: {
-                minRooms: 1,
-                maxRooms: 8,
-                minRoomWidth: 1,
-                minRoomHeight: 1,
-                maxRoomWidth: 8,
-                maxRoomHeight: 8
-            }
+new Renderer(new Generator([
+    {
+        generator: NodeMazeGenerator,
+        options: {
+            width: 20,
+            height: 20,
         }
-    ]
-}));
-console.log("rendering...");
-R.render();
+    },
+    {
+        generator: RoomGenerator,
+        options: {
+            minRooms: 1,
+            maxRooms: 8,
+            minRoomWidth: 1,
+            minRoomHeight: 1,
+            maxRoomWidth: 8,
+            maxRoomHeight: 8
+        }
+    }
+]));
