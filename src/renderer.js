e89eb88b7d8a6998cf44bfe56f043eeb403193b5
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2022-08-25 15:59:32 -0600

    Simplifies renderer pattern for easier use

--- src/renderer.js
+++ src/renderer.js
@@ -1,14 +1,9 @@
 class Renderer {
-    constructor(mazegenerator) {
-        this.mazegenerator = mazegenerator;
-        this.mazegenerator.generate();
-    }
-
-    render = () => {
-        for (let y = 0; y < this.mazegenerator.grid.height; y++) {
+    constructor(generator) {
+        for (let y = 0; y < generator.data.grid.height; y++) {
             let row = '';
-            for (let x = 0; x < this.mazegenerator.grid.width; x++) {
-                row += this.mazegenerator.grid.cells[y][x].blocked ? '\u2588' : '\u2591';
+            for (let x = 0; x < generator.data.grid.width; x++) {
+                row += generator.data.grid.cells[y][x].blocked ? '\u2588' : '\u2591';
             }
             console.log(row);
         }
