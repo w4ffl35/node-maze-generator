7fa498ba49782b87577d92335b14ea15a9e4eb20
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2022-09-03 08:50:12 -0600

    Shows stairs going up or down with an arrow

--- src/renderer.js
+++ src/renderer.js
@@ -5,9 +5,15 @@ class Renderer {
             for (let y = 0; y < generator.data.grid.height; y++) {
                 let row = '';
                 for (let x = 0; x < generator.data.grid.width; x++) {
-                    let f = generator.data.grid.cells[z][y][x].blocked ? '\u2588' : '\u2591';
-                    if (generator.data.grid.cells[z][y][x].stairs) {
-                        f = '\u2699';
+                    let cell = generator.data.grid.cells[z][y][x];
+                    let f = cell.blocked ? '\u2588' : '\u2591';
+                    if (cell.stairs) {
+                        if (cell.stairs.direction === 'up') {
+                            f = '\u25B2';
+                        }
+                        else {
+                            f = '\u25BC';
+                        }
                     }
                     row += f;
                 }
